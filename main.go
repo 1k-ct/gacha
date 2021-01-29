49769d4f04cd3222e3a21f4ddc692e3c990abc96
1k-ct <268iop@gmail.com>
2021-01-29 21:52:00 +0900

    err

--- main.go
+++ main.go
@@ -2,15 +2,20 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/1k-ct/gacha/dogacha"
 )
 
 func main() {
 	cou := 0
+	var err error
 	var hit bool
 	for !hit {
-		hit = dogacha.Gacha(100, 1) // 1%のガチャ
+		hit, err = dogacha.Gacha(100, 1) // 1%のガチャ
+		if err != nil {
+			log.Fatal(err)
+		}
 		cou++
 	}
 	fmt.Println(cou, "回目で当たりました。")
